d0d3082cd63019104f208c2bc8ed5d1ff0fec04b
Tim Smith <tsmith@chef.io>
2017-03-15 13:34:17 -0700

    Chefstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/thor-foodcritic.rb
+++ lib/thor-foodcritic.rb
@@ -1,5 +1,5 @@
-require 'thor-foodcritic/version'
-require 'foodcritic'
+require "thor-foodcritic/version"
+require "foodcritic"
 
 module ThorFoodCritic
   class Tasks < Thor
@@ -34,7 +34,7 @@ module ThorFoodCritic
       type: :array,
       aliases: "-e",
       desc: "Paths to exclude when running tests.",
-      default: ['test/**/*', 'spec/**/*', 'features/**/*']
+      default: ["test/**/*", "spec/**/*", "features/**/*"]
     desc "lint", "Run a lint test against the specified Cookbook and Role paths or otherwise your current working directory."
     def lint
       review = ::FoodCritic::Linter.new.check(
@@ -51,8 +51,8 @@ module ThorFoodCritic
 
     private
 
-      def exit_if_failure(review)
-        review.failed? ? exit(100) : nil
-      end
+    def exit_if_failure(review)
+      review.failed? ? exit(100) : nil
+    end
   end
 end
